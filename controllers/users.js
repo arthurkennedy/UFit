bfdddfe9493b274152e6790e12f120b136d2b036
James <dwest62@gmail.com>
2023-09-28 12:34:44 -0500

    added bcrypt password hash and linting

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,26 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-    const body = request.body
+	const body = request.body
 
-    const user = new User({
-        username: body.username,
-        password: body.password,
-        first_name: body.first_name,
-        last_name: body.last_name,
-        email: body.email,
-        age: body.age,
-        gender: body.gender,
-        weight: body.weight,
-    })
+	const saltRounds = 10
+	const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-    const savedUser = await user.save()
-    response.status(201).json(savedUser)
+	const user = new User({
+		username: body.username,
+		passwordHash: passwordHash,
+		first_name: body.first_name,
+		last_name: body.last_name,
+		email: body.email,
+		age: body.age,
+		gender: body.gender,
+		weight: body.weight,
+	})
+
+	const savedUser = await user.save()
+	response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
--- models/user.js
+++ models/user.js
@@ -1,24 +1,25 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    first_name: String,
-    last_name: String,
-    email: String,
-    age: {type: Number, min: 1, max: 122},
-    gender: ['Male', 'Female', 'Other'],
-    weight: Number,
-    created_at: {type: Date, default: Date.now()},
-    updated_at: {type: Date, default: Date.now()}
+	username: String,
+	passwordHash: String,
+	first_name: String,
+	last_name: String,
+	email: String,
+	age: { type: Number, min: 1, max: 122 },
+	gender: ['Male', 'Female', 'Other'],
+	weight: Number,
+	created_at: { type: Date, default: Date.now() },
+	updated_at: { type: Date, default: Date.now() }
 })
 
 userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+		delete returnedObject.passwordHash
+	}
 })
 
 module.exports = mongoose.model('User', userSchema)
